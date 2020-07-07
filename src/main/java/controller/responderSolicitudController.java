18406b7c89d3398875420b9773011882b70624b8
victor Manuel Blanco <victormanuelbm@ufps.edu.co>
2020-07-07 14:57:34 -0500

    ultimando detalles final

--- src/main/java/controller/responderSolicitudController.java
+++ src/main/java/controller/responderSolicitudController.java
@@ -35,7 +35,7 @@ public class responderSolicitudController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -50,75 +50,91 @@ public class responderSolicitudController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
         String op = request.getParameter("action");
+        System.out.println("opcion" + op);
 
         switch (op) {
             case "showresponder":
                 int id = Integer.parseInt(request.getParameter("id"));
-                Soporte tecnico1 = (Soporte) request.getSession().getAttribute("usuario");
+                System.out.println("case1" + id);
+         
+                 Usuario uss=(Usuario) request.getSession().getAttribute("usuario");
+                 Soporte tecnico1=uss.getSoporte();
+                 
                 soporteNegocio c1 = new soporteNegocio();
                 request.getSession().setAttribute("usuario", tecnico1);
                 request.setAttribute("idd", id);
+                System.out.println("case1" + c1);
+                request.getSession().setAttribute("usuario", uss);
                 request.setAttribute("list", c1.getSolicitudes());
                 request.getRequestDispatcher("./jsp/usuarioDependiencia.jsp").forward(request, response);
 
                 break;
-            case "respuesta":
-                int id2 = Integer.parseInt(request.getParameter("id"));
-                String respuesta = request.getParameter("respuesta");
-                Date fecha = new Date();
-                String estado = request.getParameter("estado");
-                Estado es = new Estado();
-                es.setDescripcion(estado);
-                String observacion = request.getParameter("observacion");
-
-                Soporte tecnico = (Soporte) request.getSession().getAttribute("usuario");
-
-                soporteNegocio c = new soporteNegocio();
-
-                Solicitud s = c.solicitudBus(id2);
-
-                if (s != null) {
-                    s.setTecnico(tecnico);
-                    s.setFecharespuesta(fecha);
-                    s.setRespuesta(respuesta);
-                    s.setEstado(es);
-                    s.setObservaciones(observacion);
-                    c.responderSolicitud(s);
-                    request.getSession().setAttribute("usuario", tecnico);
-                    request.setAttribute("list", c.getSolicitudes());
-                    request.getRequestDispatcher("./jsp/usuarioDependiencia.jsp").forward(request, response);
-
-                } else {
-                    request.getRequestDispatcher("./jsp/error.jsp").forward(request, response);
-                }
-                break;
         }
-
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+        /**
+         * Handles the HTTP <code>POST</code> method.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        @Override
+        protected void doPost
+        (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            String op = request.getParameter("action");
+            System.out.println("opcion" + op);
 
-}
+            switch (op) {
+               
+                case "respuesta":
+                    int id2 = Integer.parseInt(request.getParameter("id"));
+                    String respuesta = request.getParameter("respuesta");
+                    Date fecha = new Date();
+                    String estado = request.getParameter("estado");
+                    Estado es = new Estado();
+                    es.setDescripcion(estado);
+                    String observacion = request.getParameter("observacion");
+
+                    Soporte tecnico = (Soporte) request.getSession().getAttribute("usuario");
+
+                    soporteNegocio c = new soporteNegocio();
+
+                    Solicitud s = c.solicitudBus(id2);
+
+                    if (s != null) {
+                        s.setTecnico(tecnico);
+                        s.setFecharespuesta(fecha);
+                        s.setRespuesta(respuesta);
+                        s.setEstado(es);
+                        s.setObservaciones(observacion);
+                        c.responderSolicitud(s);
+                        request.getSession().setAttribute("usuario", tecnico);
+                        request.setAttribute("list", c.getSolicitudes());
+                        request.getRequestDispatcher("./jsp/usuarioDependiencia.jsp").forward(request, response);
+
+                    } else {
+                        request.getRequestDispatcher("./jsp/error.jsp").forward(request, response);
+                    }
+                    break;
+            }
+            System.out.println("");
+
+        }
+
+        /**
+         * Returns a short description of the servlet.
+         *
+         * @return a String containing servlet description
+         */
+        @Override
+        public String getServletInfo
+        
+            () {
+        return "Short description";
+        }// </editor-fold>
+
+    }
